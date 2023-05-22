eb6326b80ee52878817c8823b6d68611e2f609a4
thejam2 <thejam2@naver.com>
2023-05-22 18:20:20 +0900

    Detail

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 const API_KEY = process.env.API_KEY;
 
 module.exports = {
@@ -21,6 +21,10 @@ module.exports = {
       {
         source: "/api/movies",
         destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
       },
     ];
   },

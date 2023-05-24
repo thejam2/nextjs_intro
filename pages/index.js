d7287bf6324461af850058afd42263e3ebcfeef1
thejam2 <thejam2@naver.com>
2023-05-24 19:58:33 +0900

    Catch All

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,7 @@ export default function Home({ results }) {
   // }, []);
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
 
   return (
@@ -37,15 +29,7 @@ export default function Home({ results }) {
         >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link legacyBehavior
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`} legacyBehavior>
               <a>{movie.original_title}</a>
             </Link>
           </h4>
---
+++ pages/movies/[...params].js
@@ -0,0 +1,23 @@
+import Seo from "../../components/Seo";
+import { useRouter } from "next/router";
+
+export default function Detail({ params }) {
+  const router = useRouter();
+  console.log(params);
+  const [title, id] = params || [];
+  return (
+    <div>
+      <Seo title={title} />
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+export function getServerSideProps({ params: { params } }) {
+  
+  return {
+    props: {
+      params,
+    },
+  };
+}
